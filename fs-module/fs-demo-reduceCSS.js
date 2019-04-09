147cc887e0dcfee0875b1cc02b85a627d626eb18
mbw3641193 <305683054@qq.com>
2019-04-09 15:03:54 +0800

    合并压缩css 案例提交

--- fs-module/fs-demo-reduceCSS.js
+++ fs-module/fs-demo-reduceCSS.js
@@ -1,7 +1,24 @@
-let fs = require('fs');
-let {readFile} = require('./utils/fs-promise.js');
-
 //合并并且压缩CSS
-readFile('fs-module/less/1.less').then((result)=>{
+let fs = require('fs');
+let {readdir,readFile,writeFile} = require('./utils/fs-promise.js');
+
+
+readdir('fs-module/less').then((result)=>{  // 1.读取所有的css
+    // console.log(result);
+    return result.filter(item=>/\.less/i.test(item));
+}).then((result)=>{  //得到less结尾的结果
     console.log(result);
+    let arg = [];
+    result.forEach(item=>{
+        arg.push(readFile(`fs-module/less/${item}`));  //返回一个包含promise对象的数组
+    });
+    return Promise.all(arg); //返回一个数组
+}).then(result=>{
+    result = result.join('');
+    return result = result.replace(/ /g, ''); //压缩所有的空格
+    // console.log(result);
+}).then(result=>{
+    writeFile('fs-module/less/build.min.css',result) //返回一个promise
+}).then(()=>{
+    console.log('创建成功');
 });
--- fs-module/utils/fs-promise.js
+++ fs-module/utils/fs-promise.js
@@ -1,25 +1,59 @@
 let fs = require('fs');
 let path = require('path');
+//一般都会把传递的pathName进行处理；只需要传递相对路径，程序自动生成绝对目录地址
+//__dirname是创建模块的路径，path.resolve()是使用模块的路径
+let resolveName = path.resolve();
 
-let readFile = (pathName) => {
-    //一般都会把传递的pathName进行处理；只需要传递相对路径，程序自动生成绝对目录地址
-    //__dirname是创建模块的路径，path.resolve()是使用模块的路径
-    // console.log('__dirname'+__dirname);
-    // console.log(path.resolve());
+// mkdir/rmdir  2个参数 无result
 
-    path = path.resolve(path.resolve(),pathName);
+// readdir  2个参数 有result
 
-    return new Promise((resolve,reject)=>{
-        fs.readFile(path,'UTF-8',(err,result)=>{
-            if(err){
-                reject(err);
-                return;
-            }
-            resolve(result);
+// readFile  3个参数 有result
+
+// writeFile/appendFile  4个参数 无result
+
+// copyFile
+
+//1. 封装mkdir与rmdir  
+//readdir封装到这个方法里面：result判断一下，如果有result则传result，没有则是空
+//readFile封装到这个方法里面：把fs回调放到数组中，这样就可以增加第三个参数
+//copyFile封装到这个方法里面：增加copyPath，默认是空
+['mkdir','rmdir','readdir','readFile','copyFile','unlink'].forEach((item)=>{
+    module.exports[item] = (pathName,copyPath = '') => {
+        pathName = path.resolve(resolveName,pathName);
+        copyPath = path.resolve(resolveName,copyPath); //为copyFile增加
+        return new Promise((resolve,reject)=>{
+            let arg = [(err,result)=>{
+                if(err){
+                    reject(err);
+                    return;
+                }
+                resolve(result || '');
+            }]
+            item == 'readFile' ? arg.unshift('utf8') : null;
+            item == 'copyFile' ? arg.unshift(copyPath) : null;
+            fs[item](pathName,...arg);
         })
-    })
-}
+    }
+});
 
-module.exports = {
-    readFile,
-}
+//2. 封装writeFile/appendFile
+['writeFile','appendFile'].forEach((item)=>{
+    module.exports[item] = (pathName,content) => {
+        path = path.resolve(resolveName,pathName);
+
+        if(typeof content !== 'string'){
+            content = JSON.stringify(content); //content必须是字符串
+        }
+
+        return new Promise((resolve,reject)=>{
+            fs[item](path,content,'utf8',(err)=>{
+                if(err){
+                    reject(err);
+                    return;
+                }
+                resolve();
+            });
+        })
+    }
+})
